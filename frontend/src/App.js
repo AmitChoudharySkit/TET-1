dfae02078a44ead0728fd0fe3737b25a784926c2
AmitChoudharySkit <126861249+AmitChoudharySkit@users.noreply.github.com>
2024-09-17 15:56:59 +0530

    Changes Done on 17 sept

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,54 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import BookList from './components/BookList';
+// import AddBook from './components/AddBook';
+// import './App.css';
+
+// const App = () => {
+//   const [books, setBooks] = useState([]);
+
+//   useEffect(() => {
+//     axios.get('http://localhost:5000/books')
+//       .then(response => {
+//         setBooks(response.data);
+//       })
+//       .catch(error => {
+//         console.error('There was an error fetching the books!', error);
+//       });
+//   }, []);
+
+//   const addBook = (book) => {
+//     axios.post('http://localhost:5000/books', book)
+//       .then(response => {
+//         setBooks([...books, response.data]);
+//       })
+//       .catch(error => {
+//         console.error('There was an error adding the book!', error);
+//       });
+//   };
+
+//   const deleteBook = (id) => {
+//     axios.delete(`http://localhost:5000/books/${id}`)
+//       .then(() => {
+//         setBooks(books.filter(book => book.id !== id));
+//       })
+//       .catch(error => {
+//         console.error('There was an error deleting the book!', error);
+//       });
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Book Management System</h1>
+//       <AddBook onAdd={addBook} />
+//       <BookList books={books} onDelete={deleteBook} />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import BookList from './components/BookList';
@@ -6,19 +57,22 @@ import './App.css';
 
 const App = () => {
   const [books, setBooks] = useState([]);
+  
+  // Get the backend URL from an environment variable
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
 
   useEffect(() => {
-    axios.get('http://localhost:5000/books')
+    axios.get(`${backendUrl}/books`)
       .then(response => {
         setBooks(response.data);
       })
       .catch(error => {
         console.error('There was an error fetching the books!', error);
       });
-  }, []);
+  }, [backendUrl]);
 
   const addBook = (book) => {
-    axios.post('http://localhost:5000/books', book)
+    axios.post(`${backendUrl}/books`, book)
       .then(response => {
         setBooks([...books, response.data]);
       })
@@ -28,7 +82,7 @@ const App = () => {
   };
 
   const deleteBook = (id) => {
-    axios.delete(`http://localhost:5000/books/${id}`)
+    axios.delete(`${backendUrl}/books/${id}`)
       .then(() => {
         setBooks(books.filter(book => book.id !== id));
       })
