f6287930efffd7e9f5e3586f2253ce2dfa469b6e
AmitChoudharySkit <126861249+AmitChoudharySkit@users.noreply.github.com>
2024-09-22 17:30:33 +0530

    Pipeline Testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,7 +59,7 @@ const App = () => {
   const [books, setBooks] = useState([]);
   
   // Get the backend URL from an environment variable
-  const backendUrl = "http://13.201.78.75:5000";
+  const backendUrl = "http://13.126.96.212:5000";
 
   useEffect(() => {
     axios.get(`${backendUrl}/books`)
